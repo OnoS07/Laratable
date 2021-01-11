6bf31eb9368a6eb1fc610f4ef22b2f5d4c02dc0f
大野颯一郎 <soichrooono@gmail.com>
2021-01-11 13:05:09 +0900

    [Add]Model

---
+++ app/Models/Ingredient.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    protected $guarded = array('id');
+    public static $rules = array(
+        'content' => 'required|max:20',
+        'amount' => 'required|max:10'
+    );
+
+    public function recipe(){
+        $this->belongsTo('App\Model\Recipe');
+    }
+}
---
+++ app/Models/Recipe.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    # idは自動で入力(increments)
+    protected $guarded = array('id');
+
+    #バリデーションを作成 $rulesに代入
+    public static $rules = array(
+        'title' => 'required|max:30',
+        'introduction' => 'required|max:200',
+        'amount' => 'required|max:10'
+    );
+
+    #リレーションを作成
+    public function ingredients(){
+        return $this->hasMany('App\Model\Ingredient');
+    }
+
+    public function cookings(){
+        return $this->hasMany('App\Model\Cooking');
+    }
+
+}
