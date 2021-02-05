53ce9c5a12638d62a1cadee3fa7c16cdb956791f
大野颯一郎 <soichrooono@gmail.com>
2021-02-05 13:46:36 +0900

    [Fix]test_database

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+    public function test_User()
+    {
+    }
+}
